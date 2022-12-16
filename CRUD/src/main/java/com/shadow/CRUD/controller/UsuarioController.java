eae2448fb6925aebc46153691eb3d72054302bda
MayconL27 <mayconleandro2008@gmail.com>
2022-12-16 10:07:48 -0300

    Repository

--- CRUD/src/main/java/com/shadow/CRUD/controller/UsuarioController.java
+++ CRUD/src/main/java/com/shadow/CRUD/controller/UsuarioController.java
@@ -1,9 +1,58 @@
 package com.shadow.CRUD.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.shadow.CRUD.entity.Usuario;
+import com.shadow.CRUD.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class UsuarioController {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
 
+    @PostMapping(value = "salvar")
+    @ResponseBody
+    public ResponseEntity<Usuario> salvar(@RequestBody Usuario usuario) {
+
+        Usuario user = usuarioRepository.save(usuario);
+        return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+
+    }
+    @GetMapping(value = "listartodos")
+    @ResponseBody
+    public ResponseEntity<List<Usuario>> listaUsuario() {
+
+        List<Usuario> usuarios = usuarioRepository.findAll();
+        return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+
+    }
+
+    @PutMapping(value = "atualizar")
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) {
+
+        if (usuario.getId() == null) {
+            return new ResponseEntity<String>("Id não foi informado para atualização.", HttpStatus.OK);
+        }
+
+        Usuario user = usuarioRepository.saveAndFlush(usuario);
+
+        return new ResponseEntity<Usuario>(user, HttpStatus.OK);
+
+    }
+
+    @DeleteMapping(value = "delete")
+    @ResponseBody
+    public ResponseEntity<String> delete(@RequestParam Long iduser) {
+
+        usuarioRepository.deleteById(iduser);
+
+        return new ResponseEntity<String>("Usário deletado com sucesso", HttpStatus.OK);
+
+    }
 
 }
---
+++ CRUD/src/main/java/com/shadow/CRUD/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package com.shadow.CRUD.repository;
+
+import com.shadow.CRUD.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
