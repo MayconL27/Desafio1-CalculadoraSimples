936dcd319af962b95c414dfe31f0eade5043a6f7
MayconL27 <mayconleandro2008@gmail.com>
2022-12-19 15:00:09 -0300

    Adicionado Lombok

--- CRUD/src/main/java/com/shadow/CRUD/entity/Usuario.java
+++ CRUD/src/main/java/com/shadow/CRUD/entity/Usuario.java
@@ -5,30 +5,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.io.Serializable;
 
 @Entity
 @SequenceGenerator(name = "sequencia_usuario", sequenceName = "sequencia_usuario", allocationSize = 1, initialValue = 1)
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Usuario implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequencia_usuario")
     private Long id;
     private String nome;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
 }
